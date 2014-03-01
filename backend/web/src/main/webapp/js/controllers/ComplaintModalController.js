fd8617eb229c814bbb0c98b28f7cc175fad9c455
Yuri Krivochurov <yuri.krivochurov@gmail.com>
2014-03-01 21:44:41 +0700

    Кастомный select

--- backend/web/src/main/webapp/js/controllers/ComplaintModalController.js
+++ backend/web/src/main/webapp/js/controllers/ComplaintModalController.js
@@ -1,9 +1,19 @@
 function ComplaintModalController($scope, $modalInstance, $timeout, UtilsService, ComplaintService, item) {
 
+  $scope.reasons = [
+    'В объявлении обман',
+    'Фотографии не соответствуют объявлению',
+    'Неверные контактные данные',
+    'Это просто глупое баловство',
+    'Мошенники',
+    'Объявление дублируется',
+    'Другая причина'
+  ];
   $scope.complaint = {
-    reason: 0
+    reason: $scope.reasons[0]
   };
   $scope.item = item;
+
   $scope.save = function () {
     if (UtilsService.isNotEmpty($scope.item)) {
       $scope.complaint.itemId = $scope.item.id;
